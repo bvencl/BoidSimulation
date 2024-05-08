3c1d47d9ae71d902a6cd7fa30250e3ad2806ee04
Bodi.Vencel <150474053+bvencl@users.noreply.github.com>
2024-05-08 19:25:35 +0200

    c flashbackek

--- codes/src/zsource.cpp
+++ codes/src/zsource.cpp
@@ -5,55 +5,65 @@ using std::endl;
 
 int main(int argc, char *argv[])
 {
+
+    int width = 480;
+    int height = 270;
+
+    if (argc == 3)
+    {
+        sscanf(argv[1], "%d", &width);
+        sscanf(argv[2], "%d", &height);
+    }
+
     if (DEBUG_IS_ON)
     {
-        cout << "debug mode is on" << endl;
+        //     cout << "debug mode is on" << endl;
 
-        // Vektorok létrehozása
-        Vector v1(3.0, 4.0);
-        Vector v2(1.0, 2.0);
+        //     // Vektorok létrehozása
+        //     Vector v1(3.0, 4.0);
+        //     Vector v2(1.0, 2.0);
 
-        // Alapértelmezett konstruktorral létrehozott vektor
-        Vector v3;
-        Vector v4(v1);
+        //     // Alapértelmezett konstruktorral létrehozott vektor
+        //     Vector v3;
+        //     Vector v4(v1);
 
-        // Vektorok kiíratása
-        std::cout << "Vektor 1: " << v1 << std::endl;
-        std::cout << "Vektor 2: " << v2 << std::endl;
-        std::cout << "Vektor 3 (alapértelmezett): " << v3 << std::endl;
-        std::cout << "Vektor 4 (v1 alapján): " << v4 << std::endl;
+        //     // Vektorok kiíratása
+        //     std::cout << "Vektor 1: " << v1 << std::endl;
+        //     std::cout << "Vektor 2: " << v2 << std::endl;
+        //     std::cout << "Vektor 3 (alapértelmezett): " << v3 << std::endl;
+        //     std::cout << "Vektor 4 (v1 alapján): " << v4 << std::endl;
 
-        // Vektorok összeadása
-        Vector vSum = v1 + v2;
-        std::cout << "V1 + V2 = " << vSum << std::endl;
+        //     // Vektorok összeadása
+        //     Vector vSum = v1 + v2;
+        //     std::cout << "V1 + V2 = " << vSum << std::endl;
 
-        // Vektorok kivonása
-        Vector vDiff = v1 - v2;
-        std::cout << "V1 - V2 = " << vDiff << std::endl;
+        //     // Vektorok kivonása
+        //     Vector vDiff = v1 - v2;
+        //     std::cout << "V1 - V2 = " << vDiff << std::endl;
 
-        // Vektor negálása
-        Vector vNeg = -v1;
-        std::cout << "-V1 = " << vNeg << std::endl;
+        //     // Vektor negálása
+        //     Vector vNeg = -v1;
+        //     std::cout << "-V1 = " << vNeg << std::endl;
 
-        // Vektor forgatása 90 fokkal (π/2 radián)
-        Vector vRotated = v1.rotate(M_PI / 2);
-        std::cout << "V1 forgatva 90 fokkal: " << vRotated << std::endl;
+        //     // Vektor forgatása 90 fokkal (π/2 radián)
+        //     Vector vRotated = v1.rotate(M_PI / 2);
+        //     std::cout << "V1 forgatva 90 fokkal: " << vRotated << std::endl;
 
-        // Skalárral szorzás
-        Vector vScaled = v2 * 3;
-        std::cout << "V2 * 3 = " << vScaled << std::endl;
+        //     // Skalárral szorzás
+        //     Vector vScaled = v2 * 3;
+        //     std::cout << "V2 * 3 = " << vScaled << std::endl;
 
-        // Vektor hosszának ellenőrzése
-        std::cout << "V1 hossza: " << v1.getLength() << std::endl;
-        std::cout << "V2 hossza: " << v2.getLength() << std::endl;
+        //     // Vektor hosszának ellenőrzése
+        //     std::cout << "V1 hossza: " << v1.getLength() << std::endl;
+        //     std::cout << "V2 hossza: " << v2.getLength() << std::endl;
 
-        // Vektorok szögének ellenőrzése
-        double angle = v1.angleWith(v2);
-        std::cout << "V1 és V2 közötti szög (radiánban): " << angle << std::endl;
+        //     // Vektorok szögének ellenőrzése
+        //     double angle = v1.angleWith(v2);
+        //     std::cout << "V1 és V2 közötti szög (radiánban): " << angle << std::endl;
 
-        // Projekció ellenőrzése
-        Vector vProjection = v1.projectionOnto(v2);
-        std::cout << "V1 vetülete V2-re: " << vProjection << std::endl;
+        //     // Projekció ellenőrzése
+        //     Vector vProjection = v1.projectionOnto(v2);
+        //     std::cout << "V1 vetülete V2-re: " << vProjection << std::endl;
     }
 
     Flock flock;
